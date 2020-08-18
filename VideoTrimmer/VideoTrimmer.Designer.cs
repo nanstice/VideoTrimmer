0c1338186bb407cef59bf8cff022bb9d3d4d2f99
nanstice <nathan.anstice@gmail.com>
2020-08-18 22:30:55 +0100

    initial

---
+++ VideoTrimmer/VideoTrimmer.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace VideoTrimmer
+{
+    partial class VideoTrimmer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SelectedFileDisplay = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(56, 45);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(105, 23);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Select Video File";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SelectedFileDisplay});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // SelectedFileDisplay
+            // 
+            this.SelectedFileDisplay.Name = "SelectedFileDisplay";
+            this.SelectedFileDisplay.Size = new System.Drawing.Size(91, 17);
+            this.SelectedFileDisplay.Text = "No File Selected";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(594, 428);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(185, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Step 1: Select your video file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Step 2: Select number of mid-roll clips to remove";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(56, 92);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(32, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(311, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Step 3: Enter time in the input video for the output video to begin";
+            // 
+            // VideoTrimmer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "VideoTrimmer";
+            this.Text = "VideoTrimmer";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel SelectedFileDisplay;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ VideoTrimmer/VideoTrimmer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+namespace VideoTrimmer
+{
+    public partial class VideoTrimmer : Form
+    {
+        public VideoTrimmer()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = 100;
+            AppDataFolder();
+            FFMPEGCheck();
+        }
+
+        public static string userprofilepath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        public static string VideoTrimmerFolder = userprofilepath + "\\AppData\\Local\\VideoTrimmer\\";
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string file = openFileDialog1.FileName.Trim().ToString();
+            Console.WriteLine("Selected file: " + file);
+            //openFileDialog1.OpenFile();
+            SelectedFileDisplay.Text = "Selected File: " + file.ToLower();
+            FileAttributes fileattributes = File.GetAttributes(file);
+
+            //ffmpeg example - ffmpeg -i movie.mp4 -ss 00:00:03 -t 00:00:08 -async 1 cut.mp4
+
+        }
+
+        public void AppDataFolder()
+        {
+            progressBar1.Value = 10;
+            SelectedFileDisplay.Text = "User Profile Path: " + userprofilepath;
+            SelectedFileDisplay.Text = "App Path: " + userprofilepath + "\\AppData\\Local\\VideoTrimmer\\";
+            progressBar1.Value = 20;
+            Boolean videotrimmerfolderexists = Directory.Exists(VideoTrimmerFolder);
+            if (!videotrimmerfolderexists)
+            {
+                progressBar1.Value = 30;
+                SelectedFileDisplay.Text = "VideoTrimmer folder doesn't exist, creating";
+                Directory.CreateDirectory(VideoTrimmerFolder);
+                progressBar1.Value = 40;
+                Directory.CreateDirectory(VideoTrimmerFolder + "\\inputfile\\");
+                progressBar1.Value = 50;
+                Directory.CreateDirectory(VideoTrimmerFolder + "\\clips\\");
+                progressBar1.Value = 60;
+                Directory.CreateDirectory(VideoTrimmerFolder + "\\output\\");
+                progressBar1.Value = 70;
+            }
+        }
+
+        public void FFMPEGCheck()
+        {
+            Boolean ffmpegfolderexists = Directory.Exists(VideoTrimmerFolder + "\\ffmpeg");
+            if (!ffmpegfolderexists)
+            {
+                SelectedFileDisplay.Text = "FFMpeg folder doesn't exist, creating";
+                Directory.CreateDirectory(VideoTrimmerFolder + "\\ffmpeg\\");
+                progressBar1.Value = 80;
+            }
+
+            Boolean ffmpegexists = File.Exists(VideoTrimmerFolder + "\\ffmpeg\\ffmpeg.exe");
+            if (!ffmpegexists)
+            {
+                SelectedFileDisplay.Text = "FFMpeg not installed, downloading...";
+                File.Copy("ffmpeg.exe", VideoTrimmerFolder + "\\ffmpeg\\ffmpeg.exe");
+                progressBar1.Value = 90;
+            }
+
+            ffmpegfolderexists = Directory.Exists(VideoTrimmerFolder + "\\ffmpeg");
+            ffmpegexists = File.Exists(VideoTrimmerFolder + "\\ffmpeg\\ffmpeg.exe");
+
+            if (ffmpegexists && ffmpegfolderexists)
+            {
+                progressBar1.Value = 100;
+                SelectedFileDisplay.Text = "Ready";
+            }
+        }
+    }
+}
